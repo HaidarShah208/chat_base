3ba98f4efd2b520e0cb5d41f509ef80d7582230f
HaidarShah208 <fistasolutions@gmail.com>
2025-06-26 16:47:07 +0500

    fix route

--- src/routes/AppRoute.tsx
+++ src/routes/AppRoute.tsx
@@ -25,8 +25,8 @@ const AppRoute = () => {
     <>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/chatbot-widget" element={<ChatbotWidget />} />
         <Route element={<MainLayout />}>
+        <Route path="/chatbot-widget" element={<ChatbotWidget />} />
           <Route path="/dashboard/chatbot" element={<Chatbot />} />
           <Route path="/dashboard/usage" element={<Usage />} />
           <Route path="/dashboard/setting/:tab" element={<Setting />} />
